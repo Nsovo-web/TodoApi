630c5f7f79d5921dc346a05f1f4bdb2a4d5e77cb
Nsovo-web <60253275+Nsovo-web@users.noreply.github.com>
2021-04-13 17:51:09 +0200

    Add files via upload

---
+++ Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; } //functions as the unique key in a relational database
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string secret { get; set; }
+    }// The database context is the main class that coordinates Entity Framework Functionality for a data model
+}    //database context class - derived from Microsoft.EntityFrameworkCore.DbContext
